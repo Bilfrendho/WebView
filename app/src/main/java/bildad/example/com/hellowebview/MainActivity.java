0340ab3bd4184554276e2079b2046ea38e32fc8f
bilmwas <bildad.mwangi@strathmore.edu>
2017-11-16 18:40:42 +0300

    New commit on file webview

---
+++ app/src/main/java/bildad/example/com/hellowebview/MainActivity.java
@@ -0,0 +1,59 @@
+package bildad.example.com.hellowebview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+    WebView mywebview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+        mywebview=(WebView)findViewById(R.id.mywebview);
+        WebSettings webSettings=mywebview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mywebview.loadUrl("https://www.google.com/");
+        mywebview.loadUrl("https://www.bing.com/");
+        mywebview.loadUrl("https://www.facebook.com/");
+//        mywebview.setWebViewClient(new WebViewClient());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
